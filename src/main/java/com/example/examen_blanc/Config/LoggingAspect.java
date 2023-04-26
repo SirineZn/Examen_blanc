bc123d5ed65cec057f2a9b6bd1f69b54bb4819fa
SirineZn <sirine.znaidi@esprit.tn>
2023-04-26 16:49:19 +0100

    change afterReturning

--- src/main/java/com/example/examen_blanc/Config/LoggingAspect.java
+++ src/main/java/com/example/examen_blanc/Config/LoggingAspect.java
@@ -2,6 +2,7 @@ package com.example.examen_blanc.Config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class LoggingAspect {
 
-    @Before("execution(* com.example.examen_blanc.services.*.*(String))")
+    @AfterReturning("execution(* com.example.examen_blanc.services.*.*(String))")
     public void logMethodEntry(JoinPoint joinPoint) {
         String name = joinPoint.getSignature().getName();
         log.info("In method " + name + " : ");
